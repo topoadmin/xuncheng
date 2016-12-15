3612268b1550cced9f9cda56eba3bd134779ddc2
Gaoshi <13701378834@163.com>
2016-12-15 18:49:18 +0800

    项目初始化

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+	autoprefixer = require('gulp-autoprefixer'), //自动添加浏览器前缀
+	filter = require('gulp-filter'),
+	browserSync = require('browser-sync').create(), //浏览器自动刷新
+	less = require('gulp-less');
+
+//定义js,css,images文件的路径
+var paths = {
+	css: './css/*.css',
+	less: './less/**/*.less',
+	html: './*.html',
+	js:'./js/*.js'
+};
+//浏览器自动刷新
+gulp.task('browserSync', function() {
+	browserSync.init({
+//		files: "**",
+		open: false,
+		notify: false,
+		scrollProportionally: false,
+		port: 1103,
+		server: {
+			baseDir: "./"
+		}
+	});
+});
+
+// 编译less
+gulp.task('compileLess', function() {
+	gulp.src(['./less/*.less'])
+		.pipe(less())
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('./css'))
+		.pipe(filter(paths.css))
+});
+
+
+gulp.task('watchLess', function() {
+	gulp.watch(paths.less, ['compileLess']); //当所有less文件发生改变时，调用compileLess任务
+	gulp.watch([paths.css,paths.html,paths.js], browserSync.reload);
+});
+
+//定义默认任务
+gulp.task('default', ['browserSync', 'watchLess', 'compileLess']);
---
+++ js/xuncheng.js
@@ -0,0 +1,379 @@
+/* 寻城项目工具js */
+$.xuncheng = {
+	session : {
+		goodsSize: 'XC-Goods-Data-Size',
+		goodsPosition: 'XC-Goods-Position',
+	},
+	load: function(msg){
+		var msgTxt = msg || '数据加载中...';
+		var $load = $('#xc-modal-loading');
+		if(!$load.length){
+			var html = [];
+			html.push('<div class="am-modal am-modal-loading am-modal-no-btn" tabindex="-1" id="xc-modal-loading">');
+			html.push('<div class="am-modal-dialog">')
+			html.push('<div class="am-modal-hd">'+ msgTxt +'</div>')
+			html.push('<div class="am-modal-bd">')
+			html.push('<span class="am-icon-spinner am-icon-spin"></span>')
+			html.push('</div>')
+			html.push('</div>')
+			html.push('</div>')
+			$load = $(html.join('')).appendTo('body');
+		}
+		$load.modal();
+	},
+	loadEnd: function(){
+		$('#xc-modal-loading').modal('close')
+	},
+	backTop: function(scroll){
+		var $backTop = $('#xc-back-top');
+		if(scroll > 500){
+			if(!$backTop.length){
+				$backTop = $('<a href="#top" title="回到顶部" class="animated fadeInUp xc-back-top" id="xc-back-top"><i class="iconfont icon-fanhuidingbu "></i></a>').appendTo('body');
+			}
+			$backTop.show();
+		}else{
+			$backTop.hide();
+		}
+	}
+}
+
+/* 基本类型拓展，别冲突了哦  */
+Date.prototype.taDateFormat = function(format) {
+	// --格式化时间
+	// 抽离时间的各个单位
+	var obj = {
+		"M+": this.getMonth() + 1,
+		"d+": this.getDate(),
+		"h+": this.getHours(),
+		"m+": this.getMinutes(),
+		"s+": this.getSeconds(),
+		"q+": Math.floor((this.getMonth() + 3) / 3),
+		"S": this.getMilliseconds()
+	};
+	// --替换表达式
+	if(/(y+)/.test(format)) { // 由于年份是四位，所以单独抽离
+		format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+	}
+	for(var key in obj) { // 提取时间的其他单位，并组装
+		if(new RegExp("(" + key + ")").test(format)) {
+			format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? obj[key] : ("00" + obj[key]).substr(("" + obj[key]).length));
+		}
+	}
+	return format;
+}
+
+// 获取系统信息
+$.getSystem = function() {
+	var agents = ['Android', 'iPhone', 'SymbianOS', 'Windows Phone', 'iPad', 'iPod']
+	var system = 'PC';
+	agents.forEach(function(value) {
+		if(navigator.userAgent.indexOf(value) > 0) {
+			system = value;
+		}
+	})
+	return system.trim();
+}
+
+// 获取随机数
+$.getRandom = function(min, max) {
+	/*
+	 * 不传参返回随机数
+	 * 传递一个数字返回当前位数的随机数
+	 * 		$.getRandom(3) = nnn
+	 * 传递二个数字返回范围内的随机数
+	 * */
+	if(min && !max) {
+		var str = '9';
+		var str1 = '1'
+		$.each(new Array(min), function(i) {
+			str += '9';
+			str1 += '0';
+		});
+		str = parseInt(str) / 10;
+		str1 = parseInt(str1) / 10;
+		return parseInt(Math.random() * (str - str1 + 1) + str1)
+	} else if(min || min == 0 && max) {
+		return parseInt(Math.random() * (max - min + 1) + min)
+	} else {
+		return parseInt(Math.ceil(Math.random() * (new Date).getTime()))
+	}
+}
+
+// 时间格式
+$.getTime = function(options) {
+	/* option == 
+	 * option 		type		value
+	 * 	timestamp	Boolean		时间戳参数
+	 * 	timeFormat	String		时间格式
+	 * 	
+	 * 	传入type 后则是获取时间间隔值
+	 * 	type		after		后 (默认)
+	 * 				before		前
+	 * 
+	 * 	value 		number 		时间间隔
+	 * 
+	 * 	select		minute		分钟
+	 * 				day			天
+	 * */
+
+	var option = options || false;
+	var time = false;
+	if(typeof option == 'object') {
+		if(option.hasOwnProperty("type")) {
+			// 如果具备 type 参数则是获取时间间隔值
+			var newOption = $.extend(true, {
+				value: 5,
+				select: 'minute',
+				type: 'after'
+			}, option);
+			var interTimes = parseInt(newOption.value) * 60 * 1000;
+			var newDate;
+
+			// TODO 暂未处理获取传参后的时间间隔
+			if(newOption.type == 'after') {
+				newDate = new Date(Date.parse(new Date()) + interTimes);
+			} else {
+				newDate = new Date(Date.parse(new Date()) - interTimes);
+			}
+			time = newDate.taDateFormat(option.timeFormat || "yyyy-MM-dd hh:mm:ss");
+		} else if(option.hasOwnProperty("timestamp")) {
+			// 参数包含时间戳参数时
+			time = new Date(parseInt(option.timestamp)).taDateFormat(option.timeFormat || "yyyy-MM-dd hh:mm:ss");
+		} else if(option.hasOwnProperty("timeFormat")) {
+			// 参数只具备时间格式参数时
+			time = new Date().taDateFormat(option.timeFormat);
+		}
+	} else if(typeof option == 'boolean') {
+		//传入 option 为 true 时返回 object ，否则返回 String
+		if(option) {
+			var _date = new Date();
+			time = {
+				"y": _date.getFullYear(),
+				"M": _date.getMonth() + 1,
+				"d": _date.getDate(),
+				"h": _date.getHours(),
+				"m": _date.getMinutes(),
+				"s": _date.getSeconds(),
+				"ms": _date.getMilliseconds()
+			};
+		} else {
+			time = new Date().taDateFormat("yyyy-MM-dd hh:mm:ss");
+		}
+	}
+	return time
+}
+
+$(function() {
+	// 去除点击 300ms延时
+	FastClick.attach(document.body);
+
+	$.fn.ripple = function(event) {
+		var $elm = this;
+		var elmLeft = $elm.offset().left;
+		var elmTop = $elm.offset().top;
+		var elmWidth = $elm.width();
+		var elmHeight = $elm.height();
+		if(elmWidth >= elmHeight) {
+			elmHeight = elmWidth;
+		} else {
+			elmWidth = elmHeight;
+		}
+		var x = event.pageX - elmLeft - elmWidth / 2;
+		var y = event.pageY - elmTop - elmHeight / 2;
+
+		var _style = 'width:' + elmWidth + 'px;height:' + elmHeight + 'px;top:' + y + 'px;left:' + x + 'px;';
+		var $rippleSpan = $('<span class="ta-ripple-span" style="' + _style + '"></span>').appendTo($elm)
+			.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oAnimationEnd animationend', function() {
+				$rippleSpan.remove();
+			});
+
+		return $elm;
+	}
+
+	var $body = $('body');
+
+	$body.on('click', '.ta-ripple', function(event) {
+		$(this).ripple(event);
+	}).on('focus', 'input', function() {
+		mobileInputCover(this)
+	}).on('click', 'textarea', function() {
+		mobileInputCover(this)
+	})
+
+	// 解决移动端遮盖输入框
+	function mobileInputCover($elm) {
+		if($.getSystem() != 'PC') { // 解决移动端遮盖输入框事件
+			$(window).one('resize', function() {
+				$elm.scrollIntoView()
+			})
+		}
+	}
+	
+	$(window).on('scroll', function(){
+		$.xuncheng.backTop($(this).scrollTop())
+	});
+	
+	// 滚动加载
+	var $footerLazy = $('#xc-footer-lazy');
+	
+	if($footerLazy.length){
+		// 监听滚动数据加载完毕事件
+		$footerLazy.on('lazy.end', function(){
+			$footerLazy.data('xcLazy', true)
+		}).trigger('lazy.end');
+	
+		$(window).on('scroll', function() {
+			if($footerLazy.data('xcLazy')){
+				var $this = $(this);
+				var winStop = $this.scrollTop() + $this.height();
+				if(winStop > $footerLazy.offset().top){
+					// 触发事件并设置滚动监听事件失效
+					$footerLazy.data('xcLazy', false).trigger('lazy.open');
+				}
+			}
+		});
+	}
+	
+});
+
+/* 弹窗组件 */
+$(function() {
+	function DialogFx($el, options) {
+		this.$el = $el;
+		this.options = options;
+		this.$overlay = this.$el.find('.xc-dialog-overlay')
+		this.$confirmBtn = this.$el.find('[data-dialog-confirm]');
+		this.$cancelBtn = this.$el.find('[data-dialog-cancel]');
+		this._initEvents();
+		this.open()
+	}
+	// 绑定事件
+	DialogFx.prototype._initEvents = function() {
+		var self = this;
+		self.$confirmBtn.on('click', function(e) {
+			self.options.confirm(self)
+			self.close()
+		})
+		self.$cancelBtn.on('click', function(e) {
+			self.options.cancel(self)
+			self.close()
+		})
+		self.$overlay.on('touchstart', function(e) {
+			self.close()
+		})
+	}
+	// 打开
+	DialogFx.prototype.open = function() {
+		this.$el.addClass('xc-dialog-open')
+		this.options.open(this)
+	}
+	// 关闭
+	DialogFx.prototype.close = function() {
+		var self = this;
+		var xdContent = self.$el.find('.xc-dialog-content')
+
+		if($.isArray(self.options.anim)) {
+			xdContent.removeClass(self.options.anim[0]).addClass(self.options.anim[1])
+				.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+					self.$el.remove()
+				})
+		} else {
+			self.$el.removeClass('xc-dialog-open').addClass('xc-dialog-close')
+			xdContent.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				self.$el.remove()
+			})
+		}
+		self.options.end(self)
+	}
+
+	/*
+	 * options
+	 * title	标题
+	 * content	内容
+	 * anim		动画效果 0-5 或者 使用animated ['in','out']
+	 * open		打开回调
+	 * end		关闭回调
+	 * confirm	确定回调
+	 * cancel	取消回调
+	 * */
+	$.DialogFx = {
+		anim: ['susan', 'sandra', 'don', 'ken', 'alex'],
+		init: function(options) {
+			var animArr = $.DialogFx.anim;
+			var opt = $.extend(true, {
+				title: '提示',
+				content: '提示内容',
+				anim: 1,
+				open: function() {
+					return false;
+				},
+				end: function() {
+					return false;
+				},
+				confirm: function() {
+					return false;
+				},
+				cancel: function() {
+					return false;
+				}
+			}, options);
+
+			var xdialogAnim = '';
+			var xdContetAnim = '';
+			if($.isArray(opt.anim)) {
+				xdContetAnim = 'animated ' + opt.anim[0]
+			} else {
+				if(opt.anim >= animArr.length) {
+					console.error('DialogFx：没有找到的动画效果')
+					return false;
+				} else {
+					xdialogAnim = 'xc-dialog-' + animArr[opt.anim]
+				}
+			}
+			return {
+				opt: opt,
+				xdialogAnim: xdialogAnim,
+				xdContetAnim: xdContetAnim
+			}
+
+		}
+	};
+
+	$.DialogFx.alert = function(options) {
+		var newOpt = $.DialogFx.init(options);
+		var html = [];
+		html.push('<div class="xc-dialog ' + newOpt.xdialogAnim + ' ">')
+		html.push('<div class="xc-dialog-overlay"></div>')
+		html.push('<div class="xc-dialog-content ' + newOpt.xdContetAnim + ' ">')
+		html.push('<div class="am-modal-dialog">');
+		html.push('<div class="am-modal-hd">' + newOpt.opt.title + '</div>');
+		html.push('<div class="am-modal-bd">' + newOpt.opt.content + '</div>');
+		html.push('<div class="am-modal-footer"><span class="am-modal-btn" data-dialog-confirm>确定</span></div>');
+		html.push('</div>');
+		html.push('</div>')
+		html.push('</div>')
+		return new DialogFx($(html.join('')).appendTo('body'), newOpt.opt)
+	}
+
+	$.DialogFx.confirm = function(options) {
+		var newOpt = $.DialogFx.init(options);
+		var html = [];
+		html.push('<div class="xc-dialog ' + newOpt.xdialogAnim + ' ">')
+		html.push('<div class="xc-dialog-overlay"></div>')
+		html.push('<div class="xc-dialog-content ' + newOpt.xdContetAnim + ' ">')
+		html.push('<div class="am-modal-dialog">');
+		html.push('<div class="am-modal-hd">' + newOpt.opt.title + '</div>');
+		html.push('<div class="am-modal-bd">' + newOpt.opt.content + '</div>');
+		html.push('<div class="am-modal-footer">');
+		html.push('<span class="am-modal-btn" data-dialog-cancel>取消</span>');
+		html.push('<span class="am-modal-btn" data-dialog-confirm>确定</span>');
+		html.push('</div>');
+		html.push('</div>')
+		html.push('</div>')
+
+		return new DialogFx($(html.join('')).appendTo('body'), newOpt.opt)
+	};
+
+});
+
+
